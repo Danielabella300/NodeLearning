1e17ac019a18e6c86e74e61f948752584e218b37
Daniel Abella <djabella300@gmail.com>
2022-05-27 16:38:19 -0400

    Updated app.js to grab location from argument

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,25 @@
 const request = require('postman-request');
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+const process = require('process');
 
-geocode('Boston', (error, data) => 
-{
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const argLocation = process.argv[2];
+if (!argLocation || argLocation.length <= 2) {
+    console.log('Provide a legitimate ADDRESS!! greater than 3')
+} else {
+    geocode(argLocation, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(data.latitude)
+        console.log(data.longitude)
 
-forecast(40.714, -74.006, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
