c1dd4beccb165c2a905aec68db9c1e143f5d3751
Daniel Abella <djabella300@gmail.com>
2022-06-05 17:40:01 -0400

    added partials/views for handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,42 +1,72 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
 //app.com
 //app.com/help
 //app.com/about
 
 const app = express()
-const user = {
-    title: 'Weather app',
-    name: 'dj abella,'
-}
+
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebars engine and views locaiton
 app.set('views', viewsPath)
 app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index', user)
+    res.render('index', {
+        title: 'Weather',
+        name: 'DJ Abella'
+    })
 })
 
 app.get('/help', (req, res) =>
 {
-    res.render('help', user)
+    res.render('help', 
+    {
+        helpText: 'Helpful Text',
+        title: 'Help',
+        name: 'DJ Abella'
+    })
 })
 
 app.get('/about', (req,res) =>
 {
-    res.render('about', user)
+    res.render('about', {
+        title: 'About me',
+        name: 'DJ Abella'
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
-        Forecast: 'string',
-        location: 'string'
+        forecast: 'It is snowing',
+        location: 'Atlanta'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404',
+    {
+        title: 'About me',
+        name: 'DJ Abella',
+        errorMessage: 'No further pages in directory'
+    })
+})
+
+app.get('*', (req, res) => {    
+    res.render('404',
+    {
+        title: 'About me',
+        name: 'DJ Abella',
+        errorMessage: 'Page not found'
     })
 })
 
