425a67aa0ad79c9e6e1d0e640f657f0d84b25023
Daniel Abella <djabella300@gmail.com>
2022-11-02 13:37:53 -0500

    implemented app.patch(update users by id)

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -63,6 +63,18 @@ app.get('/users/:id', async (req, res) => {
     // })
 })
 
+app.patch('/users/:id', async (req,res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!user) {
+            res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {    
+        res.status(400).send(e)
+    }
+})
 app.get('/tasks', async (req, res) => {
     try {
         const task = await Task.find({})
